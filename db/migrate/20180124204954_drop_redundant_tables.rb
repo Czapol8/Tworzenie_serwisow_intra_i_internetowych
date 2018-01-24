330a094b32c39981deaeaa5dc6378f70740223bf
Czapol <mateusz.janota@gmail.com>
2018-01-24 23:51:17 +0100

    Improve site layout. Changed styles, texts and image

--- db/migrate/20180124204954_drop_redundant_tables.rb
+++
@@ -1,6 +0,0 @@
-class DropRedundantTables < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :articles_fixes
-    drop_table :datafiles
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124204954) do
+ActiveRecord::Schema.define(version: 20180124193209) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20180124204954) do
     t.datetime "updated_at"
     t.integer "user_id"
     t.string "chartFile"
+  end
+
+  create_table "datafiles", force: :cascade do |t|
+    t.string "name"
+    t.string "filen"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_datafiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
